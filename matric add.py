99e75fba02a32d7ca18544bf88c98bc88611d7f0
anusha <154488571+Anusharaju34@users.noreply.github.com>
2024-03-07 09:04:25 +0530

    Add files via upload

--- matric add.py
+++ matric add.py
@@ -1,10 +1,10 @@
-a=[[2,2],[3,3]]
-b=[[3,3],[2,2]]
+a=[[1,5,1],[3,4,1]]
+b=[[8,9,2],[7,2,2]]
 c=[[0,0],[0,0]]
 for i in range (len(a)):
     for j in range(len(b)):
         for k in range(len(b)):
-            c[i][j]=c[i][j]+a[i][k]*b[k][j]
+            c[i][j]=c[i][j][m]+a[i][k]*b[k][j]
 for i in c:
     print (i)
         
